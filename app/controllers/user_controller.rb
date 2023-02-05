33f9f5078432858856fcea3cf794413903a6cf57
jsquie <jsquie@cs.washington.edu>
2023-02-04 17:24:15 -0800

    fixed user controller so that users could be created

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,23 @@
 class UserController < ApplicationController
+  protect_from_forgery with: :null_session
+  skip_before_action :verify_authenticity_token
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user, status: :ok
+  end
+
+  def index
+    users = User.all
+    render json: users, status: :ok
+  end
+
+  def create
+    user = User.create!(user_params)
+    render json: user, status: :ok
+  end
+  
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
