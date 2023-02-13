81943430e876db690ff65f2b696487313614e8a5
jsquie <jsquie@cs.washington.edu>
2023-02-13 12:44:31 -0800

    user controller

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,2 +1,12 @@
 class EventController < ApplicationController
+
+  # POST /event/{id}
+  def create
+    @user 
+  end
+
+
+  # DELETE /event/{id}
+  # PATCH /event/{id}
+
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,22 +2,31 @@ class UserController < ApplicationController
   protect_from_forgery with: :null_session
   skip_before_action :verify_authenticity_token
 
+  # GET /user/{id}
   def show
     @user = User.find(params[:id])
     render json: @user, status: :ok
   end
 
+  # GET /user
   def index
     users = User.all
     render json: users, status: :ok
   end
 
+  # POST /user
   def create
     user = User.create!(user_params)
     render json: user, status: :ok
+  end
+
+  # DELETE /user/{id}
+  def destroy
+    @user.destory
   end
   
   def user_params
     params.require(:user).permit(:name, :email)
   end
+
 end
