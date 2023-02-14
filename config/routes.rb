8217650f901596e2df7a082c8fd4c00f3560ca92
jsquie <jsquie@cs.washington.edu>
2023-02-14 13:50:50 -0800

    removed conflict, added constraint model

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :user, only: [:index, :show, :create, :update, :delete]
   resources :task, only: [:index, :show, :create, :update, :delete]
   resources :event, only: [:index, :show, :create, :update, :delete]
-  resources :conflict, only: [:index, :show, :create, :update, :delete]
+  resources :constraint, only: [:index, :show, :create, :update, :delete]
   # Defines the root path route ("/")
   # root "articles#index"
 end
