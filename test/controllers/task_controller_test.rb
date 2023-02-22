203b696da5963587a556f034978fa2e6dcde15c6
mjpreigh <74576265+mjpreigh@users.noreply.github.com>
2023-02-21 21:55:22 -0800

    task controller testing work in progress

--- test/controllers/task_controller_test.rb
+++ test/controllers/task_controller_test.rb
@@ -4,4 +4,24 @@ class TaskControllerTest < ActionDispatch::IntegrationTest
    test "the truth" do
      assert true
    end
+   
+   test "task should have title and user_id" do
+     @task = Task.new
+     assert_not @task.save, "Saved task without title and user_id"
+   end
+   
+   test "task create" do
+     @task = Task.create(:name => "task", :priority => 1, :category => 1)
+     tid = @task.id
+     assert_equal(Task.find(tid), @task)
+   end
+   
+   test "task destroy" do
+     @task = Task.create(:name => "task", :priority => 1, :category => 1)
+     tid = @task.id
+     assert_equal(Task.find(tid), @task)
+     Task.destroy(tid)
+     assert_raise(Exception) {Task.find(tid)}
+   end
+   
 end
