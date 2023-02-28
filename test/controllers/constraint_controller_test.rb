61ba760c3333ca356701f1a142e24a54696ba82c
jsquie <jsquie@cs.washington.edu>
2023-02-27 21:44:58 -0800

    made some minor fixes for consistency in user tests, and implemented controller tests successfully

--- test/controllers/constraint_controller_test.rb
+++ test/controllers/constraint_controller_test.rb
@@ -114,60 +114,88 @@ class ConstraintControllerTest < ActionDispatch::IntegrationTest
 
   end
 
-  # # update constraint duration
-  # test "update constraint duration" do
-  #   tokenAuth = sign_in_api_user(users(:user_task_constraint).email)
-  #   route = "/task/" + tasks(:task_with_constraint).id.to_s + "/constraint"
-  #
-  #   patch route,
-  #     headers: { 'Authorization': tokenAuth },
-  #     params: { constraint: { dueTime: , duration: } }
-  #
-  # end
-  #
-  # # update both duetime and duration of existing constraint
-  # test "update both duetime and duration of existing constraint" do
-  #   tokenAuth = sign_in_api_user(users(:user_task_constraint).email)
-  #   route = "/task/" + tasks(:task_with_constraint).id.to_s + "/constraint"
-  #
-  #   patch route,
-  #     headers: { 'Authorization': tokenAuth },
-  #     params: { constraint: { dueTime: , duration: } }
+  # update constraint duration
+  test "update constraint duration" do
+    tokenAuth = sign_in_api_user(users(:user_task_constraint).email)
+    route = "/task/" + tasks(:task_with_constraint).id.to_s + "/constraint"
 
-  # end
-  #
-  # # update with no args (error)
-  # test "update with no args (error)" do
-  #   tokenAuth = sign_in_api_user(users(:user_task_constraint).email)
-  #   route = "/task/" + tasks(:task_with_constraint).id.to_s + "/constraint"
-  #
-  #   patch route,
-  #     headers: { 'Authorization': tokenAuth },
-  #     params: { constraint: { dueTime: , duration: } }
-  #
-  # end
-  #
-  # # update with non-existant task (error)
-  # test "update with non-existant task (error)" do
-  #   tokenAuth = sign_in_api_user(users(:user_task_constraint).email)
-  #   route = "/task/" + tasks(:task_with_constraint).id.to_s + "/constraint"
-  #
-  #   patch route,
-  #     headers: { 'Authorization': tokenAuth },
-  #     params: { constraint: { dueTime: , duration: } }
-  #
-  # end
-  #
-  # # update non existant constraint
-  # test "update non existant constraint" do
-  #   tokenAuth = sign_in_api_user(users(:user_task_constraint).email)
-  #   route = "/task/" + tasks(:task_with_constraint).id.to_s + "/constraint"
-  #
-  #   patch route,
-  #     headers: { 'Authorization': tokenAuth },
-  #     params: { constraint: { dueTime: , duration: } }
-  #
-  # end
-#
+    newDuration = 20
+
+    patch route,
+      headers: { 'Authorization': tokenAuth },
+      params: { constraint: { duration: newDuration } }
+
+    assert_response :success
+
+    @updatedConstraint = Task.find(tasks(:task_with_constraint).id).constraint
+
+    assert_equal newDuration, @updatedConstraint.duration
+
+  end
+
+  # update both duetime and duration of existing constraint
+  test "update both duetime and duration of existing constraint" do
+    tokenAuth = sign_in_api_user(users(:user_task_constraint).email)
+    route = "/task/" + tasks(:task_with_constraint).id.to_s + "/constraint"
+
+    newDuration = 20;
+    newDueTime = "1992-02-21 00:00:00 UTC"
+
+    patch route,
+      headers: { 'Authorization': tokenAuth },
+      params: { constraint: { dueTime: newDueTime, duration: newDuration} }
+
+    assert_response :success
+    @updatedConstraint = Task.find(tasks(:task_with_constraint).id).constraint
+
+    assert_equal newDuration, @updatedConstraint.duration
+    assert_equal newDueTime, @updatedConstraint.dueTime.to_s
+
+  end
+
+  # update with no args (error)
+  test "update with no args (error)" do
+    tokenAuth = sign_in_api_user(users(:user_task_constraint).email)
+
+    route = "/task/" + tasks(:task_with_constraint).id.to_s + "/constraint"
+
+    assert_raises() do
+      patch route,
+        headers: { 'Authorization': tokenAuth },
+        params: { constraint: {  } }
+    end
+
+  end
+
+  # update with non-existant task (error)
+  test "update with non-existant task (error)" do
+
+    tokenAuth = sign_in_api_user(users(:user_task_constraint).email)
+    route = "/task/9999/constraint"
+
+    assert_raises() do
+      patch route,
+        headers: { 'Authorization': tokenAuth },
+        params: { constraint: { duration: 10, dueTime: Time.now  } }
+    end
+
+  end
+
+  # update non existant constraint
+  test "update non existant constraint" do
+    tokenAuth = sign_in_api_user(users(:user_task_constraint).email)
+
+    route = "/task/" + tasks(:task_noconstraint).id.to_s + "/constraint"
+
+    newTime = "2023-02-21 00:00:00 UTC" 
+    newDuration = 10
+
+    assert_raises() do
+      patch route,
+        headers: { 'Authorization': tokenAuth },
+        params: { constraint: { dueTime: newTime, duration: newDuration } }  
+    end
+  end
+
 end
 
